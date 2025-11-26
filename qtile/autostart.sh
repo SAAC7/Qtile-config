c333db6e509815cc085ffe6d1c93aadd46a75b5d
SAAC7_Hybrid <saulabraham881@gmail.com>
2025-11-26 15:38:07 -0600

    i add the code of autostart

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-# ~/.config/qtile/autostart.sh
 #picom -b
+thunar --daemon &
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 nm-applet &
 blueman-applet &
 udiskie -t &
 copyq &
 flameshot &
+killall dunst 2>/dev/null
 dunst &
 nitrogen --restore &
-xss-lock -- i3lock &
+xss-lock -- i3lock -c 000000 &
