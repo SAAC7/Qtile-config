f3dd5efb72e845c33000be560632657daf6cbeca
SAAC7_Hybrid <saulabraham881@gmail.com>
2026-01-29 15:16:11 -0600

    I add the scrip to add a mic for monitoring on obs

---
+++ optional/mic_obs.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Crea el virtual mic de OBS si no existe
+SINK_NAME="OBS_VirtualMic"
+
+# Verifica si ya existe
+if ! pactl list short sinks | grep -q "$SINK_NAME"; then
+    pactl load-module module-null-sink sink_name=$SINK_NAME sink_properties=device.description=$SINK_NAME
+fi
