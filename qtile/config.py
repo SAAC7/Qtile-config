85b28cbe218c97b08c3e049520ff59facef759d0
SAAC7 <saulabraham881@gmail.com>
2026-05-05 19:18:55 -0600

    i add some changes an configs

--- qtile/config.py
+++ qtile/config.py
@@ -152,7 +152,7 @@ keys = [
     Key([mod],"v", lazy.window.toggle_minimize(), desc="minimizar ventana"),
     Key([mod, "shift"],"f", lazy.window.disable_floating(), desc="Quitar floatingg a la ventana"),
     Key([mod, "shift"], "v", lazy.window.toggle_maximize(), desc="maximizar ventana"),
-    Key(["mod1"], "n", lazy.spawn("dunstctl set-paused toggle"), desc="Toggle notifications"),
+#    Key(["mod1"], "n", lazy.spawn("dunstctl set-paused toggle"), desc="Toggle notifications"),
     Key(["mod1"], "space", lazy.next_screen(), desc="Move to other screen"),
 
     # Bloqueador de pantalla
